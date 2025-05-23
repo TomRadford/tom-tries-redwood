9c5e3039f2c4b730809ee0e62c618bcd4d201eeb
Tom Radford <tom@teamgeek.io>
2025-05-23 16:25:47 +0200

    redirect login if existing user

--- src/app/pages/user/routes.ts
+++ src/app/pages/user/routes.ts
@@ -3,7 +3,19 @@ import { Login } from "./Login";
 import { sessions } from "@/session/store";
 
 export const userRoutes = [
-  route("/login", [Login]),
+  route("/login", [
+    ({ ctx }) => {
+      const headers = new Headers();
+      headers.set("Location", "/protected");
+      if (ctx.user) {
+        return new Response(null, {
+          status: 302,
+          headers,
+        });
+      }
+    },
+    Login,
+  ]),
   route("/logout", async function ({ request }) {
     const headers = new Headers();
     await sessions.remove(request, headers);
